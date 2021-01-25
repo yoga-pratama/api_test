dbd3536a63167b94cce6ecb6002148abbd9fa507
Yoga Pratama <yogamadness@gmail.com>
2021-01-25 13:42:38 +0700

    commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    //
+    public function getUser(){
+
+        $user = User::all();
+        $user2 = User::where('username' ,'like', 'j%')->get();
+        
+
+        return response()->json($user2);
+
+    }
+
+
+    public function addUser(Request $request){
+
+        $name = $request->name;
+        $username = $request->username;
+        $password = $request->password;
+        $email = $request->email;
+
+        $addUser =  new User();
+        $addUser->name = $name;
+        $addUser->username = $username;
+        $addUser->password = $password;
+        $addUser->email = $email;
+        $addUser->save();
+
+        return response()->json([
+            'message' => 'Data Tersimpan'
+        ],201);
+
+    }
+}
